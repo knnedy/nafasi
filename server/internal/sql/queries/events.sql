5cf47171b120ed3dda0d2e039a0f2c49a99b11ad
knnedy <kennymwendwadev@gmail.com>
2026-04-10 23:38:00 +0300

    feat(queries): add events sql queries

---
+++ server/internal/sql/queries/events.sql
@@ -0,0 +1,67 @@
+-- name: CreateEvent :one
+INSERT INTO "events" (
+    "organiser_id",
+    "title",
+    "slug",
+    "description",
+    "location",
+    "venue",
+    "banner_url",
+    "starts_at",
+    "ends_at",
+    "status",
+    "is_online",
+    "online_url"
+) VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12
+) RETURNING *;
+
+-- name: GetEventById :one
+SELECT * FROM "events" WHERE "id" = $1;
+
+-- name: GetEventBySlug :one
+SELECT * FROM "events" WHERE "slug" = $1;
+
+-- name: GetEventsByOrganiser :many
+SELECT * FROM "events" 
+WHERE "organiser_id" = $1
+ORDER BY "created_at" DESC;
+
+-- name: GetPublishedEvents :many
+SELECT * FROM "events"
+WHERE "status" = 'PUBLISHED'
+ORDER BY "starts_at" ASC;
+
+-- name: GetUpcomingEvents :many
+SELECT * FROM "events"
+WHERE "status" = 'PUBLISHED'
+AND "starts_at" > NOW()
+ORDER BY "starts_at" ASC;
+
+-- name: UpdateEvent :one
+UPDATE "events"
+SET
+    "title"      = $2,
+    "slug"       = $3,
+    "description"= $4,
+    "location"   = $5,
+    "venue"      = $6,
+    "banner_url" = $7,
+    "starts_at"  = $8,
+    "ends_at"    = $9,
+    "is_online"  = $10,
+    "online_url" = $11,
+    "updated_at" = NOW()
+WHERE "id" = $1
+RETURNING *;
+
+-- name: UpdateEventStatus :one
+UPDATE "events"
+SET
+    "status"     = $2,
+    "updated_at" = NOW()
+WHERE "id" = $1
+RETURNING *;
+
+-- name: DeleteEvent :exec
+DELETE FROM "events" WHERE "id" = $1;
