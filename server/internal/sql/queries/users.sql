b5616d94a0512ef55cd3d19443e6a6a15bcbdc0f
knnedy <kennymwendwadev@gmail.com>
2026-04-10 23:35:31 +0300

    feat(queries): add user sql queries

---
+++ server/internal/sql/queries/users.sql
@@ -0,0 +1,43 @@
+-- name: CreateUser :one
+INSERT INTO "users" (
+    "name",
+    "email",
+    "password_hash",
+    "role"
+) VALUES (
+    $1, $2, $3, $4
+) RETURNING *;
+
+-- name: GetUserById :one
+SELECT * FROM "users" WHERE "id" = $1;
+
+-- name: GetUserByEmail :one
+SELECT * FROM "users" WHERE "email" = $1;
+
+-- name: UpdateUserProfile :one
+UPDATE "users"
+SET
+    "name" = $2,
+    "email" = $3,
+    "updated_at" = NOW()
+WHERE "id" = $1
+RETURNING *;
+
+-- name: UpdateUserPassword :one
+UPDATE "users"
+SET
+    "password_hash" = $2,
+    "updated_at" = NOW()
+WHERE "id" = $1
+RETURNING *;
+
+-- name: UpdateUserAvatar :one
+UPDATE "users"
+SET
+    "avatar_url" = $2,
+    "updated_at" = NOW()
+WHERE "id" = $1
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM "users" WHERE "id" = $1;
