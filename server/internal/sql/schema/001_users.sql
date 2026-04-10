29ca565bfbf012fe7faa87c534668c22684c684f
knnedy <kennymwendwadev@gmail.com>
2026-04-10 16:18:30 +0300

    refactor(schema): update users table to include default values and constraints

--- server/internal/sql/schema/001_users.sql
+++ server/internal/sql/schema/001_users.sql
@@ -2,15 +2,16 @@
 CREATE TYPE user_role AS ENUM ('ATTENDEE', 'ORGANISER', 'ADMIN');
 
 CREATE TABLE "users" (
-    "id" UUID PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL UNIQUE,
+    "id"            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "name"          TEXT NOT NULL,
+    "email"         TEXT NOT NULL,
     "password_hash" TEXT NOT NULL,
-    "role" user_role NOT NULL,
-    "avatar_url" TEXT,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
-    "updated_at" TIMESTAMP(3)
-)
+    "role"          user_role NOT NULL DEFAULT 'ATTENDEE',
+    "avatar_url"    TEXT,
+    "created_at"    TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "updated_at"    TIMESTAMP(3),
+    CONSTRAINT "users_email_key" UNIQUE ("email")
+);
 
 -- +goose Down
 DROP TABLE "users";
