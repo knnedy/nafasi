89814ddda49c121e97b52ea8d4e1e1d499e5cace
knnedy <kennymwendwadev@gmail.com>
2026-04-10 16:16:06 +0300

    feat(schema): add users sql schema

---
+++ server/internal/sql/schema/001_users.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+CREATE TYPE user_role AS ENUM ('ATTENDEE', 'ORGANISER', 'ADMIN');
+
+CREATE TABLE "users" (
+    "id" UUID PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL UNIQUE,
+    "password_hash" TEXT NOT NULL,
+    "role" user_role NOT NULL,
+    "avatar_url" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP(3)
+)
+
+-- +goose Down
+DROP TABLE "users";
+DROP TYPE user_role;
