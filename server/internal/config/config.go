b00ffb7c5e0dd44bb13caaa123d4f1352338a759
knnedy <kennymwendwadev@gmail.com>
2026-04-18 23:40:49 +0300

    feat(mpesa): add Mpesa ENV variables to the config

--- server/internal/config/config.go
+++ server/internal/config/config.go
@@ -8,10 +8,16 @@ import (
 )
 
 type Config struct {
-	DBUrl     string
-	JWTSecret string
-	Port      string
-	Env       string
+	DBUrl                 string
+	JWTSecret             string
+	Port                  string
+	Env                   string
+	MPESA_CONSUMER_KEY    string
+	MPESA_CONSUMER_SECRET string
+	MPESA_SHORTCODE       string
+	MPESA_PASSKEY         string
+	MPESA_ENV             string
+	MPESA_CALLBACK_URL    string
 }
 
 func Load() Config {
@@ -21,10 +27,16 @@ func Load() Config {
 	}
 
 	return Config{
-		DBUrl:     mustGet("DATABASE_URL"),
-		JWTSecret: mustGet("JWT_SECRET"),
-		Port:      getOrDefault("PORT", "8080"),
-		Env:       getOrDefault("ENV", "development"),
+		DBUrl:                 mustGet("DATABASE_URL"),
+		JWTSecret:             mustGet("JWT_SECRET"),
+		Port:                  getOrDefault("PORT", "8080"),
+		Env:                   getOrDefault("ENV", "development"),
+		MPESA_CONSUMER_KEY:    mustGet("MPESA_CONSUMER_KEY"),
+		MPESA_CONSUMER_SECRET: mustGet("MPESA_CONSUMER_SECRET"),
+		MPESA_SHORTCODE:       mustGet("MPESA_SHORTCODE"),
+		MPESA_PASSKEY:         mustGet("MPESA_PASSKEY"),
+		MPESA_ENV:             getOrDefault("MPESA_ENV", "sandbox"),
+		MPESA_CALLBACK_URL:    mustGet("MPESA_CALLBACK_URL"),
 	}
 }
 
